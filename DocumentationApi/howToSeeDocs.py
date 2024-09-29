cfd6d82db584a042dabc29f30c99a02ed742ecfa
ADILRADIDI <adulradidi@gmail.com>
2024-09-29 13:13:15 +0100

    fin fin

---
+++ DocumentationApi/howToSeeDocs.py
@@ -0,0 +1,5 @@
+# start serve localhost:8000
+# and go to browser set 
+#  ---- > http://127.0.0.1:8000/docs#endpoints-GETapi-documentation
+# documentation website :
+#  https://scribe.readthedocs.io/en/latest/
---
+++ Figma/linkMyFigmaOnline.py
@@ -0,0 +1,2 @@
+# link figma online is public are you okay : 
+# https://www.figma.com/design/SMbF1uQWz9a7qcq1QmZTNg/fill-rouge?node-id=0-1&node-type=canvas
