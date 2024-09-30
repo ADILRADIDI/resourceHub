4624a991c7eb3bfd564edec85cfa7643797135b3
ADILRADIDI <adulradidi@gmail.com>
2024-09-30 18:42:56 +0100

    a

---
+++ Dossier de l'Implémentation/dépôt_GitHub.py
@@ -0,0 +1,5 @@
+# Lien vers le dépôt GitHub de l'implémentation
+# https://github.com/ADILRADIDI/resourceHub
+
+# Lien du projet hébergé
+# 
--- uml/task.py
+++ uml/task.py
@@ -1,10 +1,7 @@
-# link of three uml 
-
-# -> diagram de class :  
-# 
+# link of three uml is public 
 
 # -> diagram de use case : 
-# 
+# https://lucid.app/lucidspark/019c26cf-ebd0-4eeb-951e-276738721201/edit?viewport_loc=-5320%2C-2573%2C11469%2C5473%2C0_0&invitationId=inv_8326ad45-903e-43a8-a886-2d38e4cd067a
 
-# ->diagram de sequence : 
-# 
+# -> diagram de class :  
+# https://lucid.app/lucidchart/b3646b75-e0a8-4e22-87c0-6e5bb53a50bc/edit?viewport_loc=546%2C-1500%2C3312%2C1533%2CHWEp-vi-RSFO&invitationId=inv_fa0e8da5-728b-4ddb-a6b5-1c49580ce11a
